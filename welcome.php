60d1e43de9c21ba60de68b8502a5de2a267b6fe1
nitishv509 <47640921+nitishv509@users.noreply.github.com>
2019-04-01 09:01:42 +0530

    Login system

---
+++ welcome.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+include("checklogin.php");
+check_login();
+
+	
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Welcome </title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/heroic-features.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Welcome !</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#"><?php echo $_SESSION['name'];?></a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                  
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <iframe src="http://notesacademy.tk" style="border: none; height: 1000px; width: 100%;" frameborder="0"> </iframe>
+
+        
+
+      
+
+
+        
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
